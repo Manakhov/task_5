1d8f7c5b0be1b49c2b473d916df33087329ca6af
Svyatoslav <manakhovsvyatoslav@gmail.com>
2020-08-30 16:29:44 +0300

    Additional task 1

--- task_5.py
+++ task_5.py
@@ -1,8 +1,18 @@
 import re
 
 
-mail_user = input("Please enter your mail: ")
-if re.fullmatch(r"[\da-zA-Z]+@[\da-zA-Z]+[.][\da-zA-Z]+", mail_user):
-    print("Good")
-else:
-    print("Bad")
+vault = {'whitelist': ['good@good.good', 'notbad@notbad.notbad'],
+         'blacklist': ['penis@penis.penis', 'bad@bad.bad']}
+print("Write 'exit' to exit.")
+while True:
+    mail_user = input("Please enter your mail: ")
+    if mail_user == 'exit':
+        break
+    if re.fullmatch(r"[\da-zA-Z]+@[\da-zA-Z]+[.][\da-zA-Z]+", mail_user):
+        print("Good.")
+        if mail_user in vault['whitelist']:
+            print("Your mail in whitelist.")
+        elif mail_user in vault['blacklist']:
+            print("Your mail in blacklist.")
+    else:
+        print("Bad.")
