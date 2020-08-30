5a9d961641966763fd90f5a11949f575a69f6d2c
Svyatoslav <manakhovsvyatoslav@gmail.com>
2020-08-30 18:22:13 +0300

    Additional task 2

--- task_5.py
+++ task_5.py
@@ -1,6 +1,33 @@
 import re
 
 
+def check_mail(mail, dict_white_black):
+    if re.fullmatch(r"[\da-zA-Z]+@[\da-zA-Z]+[.][\da-zA-Z]+", mail):
+        print("Good.")
+        if mail in dict_white_black['whitelist']:
+            print("Your mail in whitelist.")
+        elif mail in dict_white_black['blacklist']:
+            print("Your mail in blacklist.")
+        return True
+    else:
+        return False
+
+
+def check_password(password):
+    if re.search(r"\s", password):
+        print("The password must not be any non-displayable symbols.")
+        return False
+    elif len(password) < 8:
+        print("The password length must be at least 8.")
+        return False
+    elif len(re.findall(r"\d", password)) < 3 or \
+            len(re.findall(r"[a-zA-Z]", password)) < 3 or \
+            len(re.findall(r"[\W_]", password)) < 1:
+        print("The password must be at least 3 digits, 3 letters, 1 symbol.")
+        return False
+    return True
+
+
 vault = {'whitelist': ['good@good.good', 'notbad@notbad.notbad'],
          'blacklist': ['penis@penis.penis', 'bad@bad.bad']}
 print("Write 'exit' to exit.")
@@ -8,11 +35,10 @@ while True:
     mail_user = input("Please enter your mail: ")
     if mail_user == 'exit':
         break
-    if re.fullmatch(r"[\da-zA-Z]+@[\da-zA-Z]+[.][\da-zA-Z]+", mail_user):
-        print("Good.")
-        if mail_user in vault['whitelist']:
-            print("Your mail in whitelist.")
-        elif mail_user in vault['blacklist']:
-            print("Your mail in blacklist.")
+    if check_mail(mail_user, vault):
+        while True:
+            password_user = input("Please enter your password: ")
+            if check_password(password_user):
+                break
     else:
         print("Bad.")
