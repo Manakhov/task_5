680dcb469e2bee2af97568550fb83c74d99bf7e1
Svyatoslav <manakhovsvyatoslav@gmail.com>
2020-08-30 20:20:16 +0300

    Additional task 6

--- task_5.py
+++ task_5.py
@@ -3,11 +3,13 @@ import re
 
 def check_mail(mail, dict_white_black, consent):
     if re.fullmatch(r"[\da-zA-Z]+@[\da-zA-Z]+[.][\da-zA-Z]+", mail):
-        if mail in dict_white_black['whitelist']:
-            print("Your mail in whitelist.")
-        elif mail in dict_white_black['blacklist']:
-            print("Your mail in blacklist.")
-            if consent == 'yes':
+        if mail in dict_white_black['white']:
+            print("Your mail in white list.")
+            if consent == 'white':
+                return False
+        elif mail in dict_white_black['black']:
+            print("Your mail in black list.")
+            if consent == 'yes' or consent == 'black':
                 return False
         else:
             print("Your mail matches pattern.")
@@ -32,13 +34,25 @@ def check_password(password):
     return True
 
 
-vault = {'whitelist': ['good@good.good', 'notbad@notbad.notbad'],
-         'blacklist': ['penis@penis.penis', 'bad@bad.bad']}
+vault = {'white': ['good@good.good', 'notbad@notbad.notbad'],
+         'black': ['penis@penis.penis', 'bad@bad.bad']}
 vault_user = {}
 while True:
-    answer_user = input("Use white and black lists? (yes/no/exit)\n")
+    answer_user = input("Use white and black lists? (yes/no/debug/exit)\n")
     if answer_user == 'exit':
         break
+    elif answer_user == 'debug':
+        while True:
+            answer_user_2 = input("Debug white or black list? (white/black/exit)\n")
+            if answer_user_2 == 'exit':
+                break
+            elif answer_user_2 != 'white' and answer_user_2 != 'black':
+                continue
+            mail_user_2 = input("Please enter added mail: ")
+            if check_mail(mail_user_2, vault, answer_user_2):
+                vault[answer_user_2] = mail_user_2
+                print("The mail is added.")
+        continue
     elif answer_user != 'yes' and answer_user != 'no':
         continue
     mail_user = input("Please enter your mail: ")
@@ -46,14 +60,14 @@ while True:
         print("This mail is already registered.")
         password_user = input("Please enter your password: ")
         if password_user == vault_user[mail_user]:
-            print("Your password is correct")
+            print("Your password is correct.")
         else:
-            print("Your password is not correct")
+            print("Your password is not correct.")
         continue
     if check_mail(mail_user, vault, answer_user):
         while True:
             password_user = input("Please enter your password: ")
             if check_password(password_user):
                 vault_user[mail_user] = password_user
-                print("Your mail is registered")
+                print("Your mail is registered.")
                 break
